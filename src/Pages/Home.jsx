3fad966359cec58edab69e0ac8c2178182a00b8c
Shubham <Shubham101021@gmail.com>
2022-08-26 20:20:37 +0530

    done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -22,9 +22,8 @@ const Home = () => {
         //     setProducts(data);
         // };
         // fetchProducts();
-    });
+    }, []);
 
-    console.log(data);
 
 
     const handleAdd = (product) => {
@@ -235,22 +234,6 @@ const Home = () => {
                             )
                         })}
 
-
-                        <div className="col-lg-3 col-md-4 col-sm-6 mix fastfood vegetables">
-                            <div className="featured__item">
-                                <div className="featured__item__pic set-bg" style={{ backgroundImage: "url(../assets/img/featured/feature-8.jpg)" }}>
-                                    <ul className="featured__item__pic__hover">
-                                        <li><a href="/"><i className="fa fa-heart"></i></a></li>
-                                        <li><a href="/"><i className="fa fa-retweet"></i></a></li>
-                                        <li><a href="/"><i className="fa fa-shopping-cart"></i></a></li>
-                                    </ul>
-                                </div>
-                                <div className="featured__item__text">
-                                    <h6><a href="/">Crab Pool Security</a></h6>
-                                    <h5>$30.00</h5>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </section>
